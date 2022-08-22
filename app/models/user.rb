83efbb51da121105a710353b60e09a83e6889734
Timothy Omoniyi <olayinkatimothy102@gmail.com>
2022-08-22 17:55:59 +0200

    almost done

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w.]+\w+@[\w.]+\.[\w.]+\w{2,}\z/,
-    message: "please enter a valid email" }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w.]+\w+@[\w.]+\.[\w.]+\w{2,}\z/, message: 'please enter a valid email' }
   validates :password, presence: true, length: { minimum: 6 }
-
 end
+
