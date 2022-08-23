f0a8fac2af2e98c2b9f1cf2b0e8ad768c28bc044
Alice Bardon <abardon.catineau@gmail.com>
2022-08-23 13:23:36 +0200

    commit before lunch

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -4,11 +4,25 @@ class WorkspacesController < ApplicationController
   end
 
   def show
+    @workspace = Workspace.find(params[:id])
   end
 
   def new
+    @workspace = Workspace.new
   end
 
   def create
+    @workspace = Workspace.new(ws_params)
+    if @workspace.save
+      redirect_to workspace_path(@workspace)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def ws_params
+    params.require(:workspace).permit(:name, :address, :neighborhood, :price, :user)
   end
 end
