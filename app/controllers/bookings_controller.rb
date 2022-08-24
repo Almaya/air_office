d1475670e835ed4020227b708fb0c17d20175cae
Maya Eyal <mayalindae@gmail.com>
2022-08-24 16:02:26 +0200

    switched to Cloudinary storage on local and dev

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,6 +19,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.workspace = @workspace
+    raise
     if @booking.save
       redirect_to workspace_booking_path(@workspace, @booking), notice: 'Booking successfully created'
     else
