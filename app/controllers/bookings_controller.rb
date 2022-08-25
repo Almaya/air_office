9ed23f905eb7ce017fb8a4f09ea831cb29ddfc31
Timothy Omoniyi <olayinkatimothy102@gmail.com>
2022-08-25 11:42:42 +0200

    searching

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,11 @@ class BookingsController < ApplicationController
 
   def index
     @bookings = @workspace.bookings
+    if params[:query].present?
+      @workspaces = Workspace.where(name: params[:query])
+    else
+      @workspaces = Workspace.all
+    end
   end
 
   def show
