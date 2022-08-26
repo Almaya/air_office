787ac21ddcfa79d8108b489d94ba93dd5cee0839
Maya Eyal <mayalindae@gmail.com>
2022-08-26 14:48:43 +0200

    nested bookings under main page except for new and create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
-  before_action :find_workspace
+  before_action :find_workspace, only: %i[new create]
   before_action :find_booking, only: %i[show edit update]
 
   def index
@@ -23,7 +23,7 @@ class BookingsController < ApplicationController
     @booking.user = current_user
     @booking.workspace = @workspace
     if @booking.save
-      redirect_to workspace_booking_path(@workspace, @booking), notice: 'Booking successfully created'
+      redirect_to booking_path(@booking), notice: 'Booking successfully created'
     else
       render :new, status: :unprocessable_entity
     end
@@ -34,26 +34,26 @@ class BookingsController < ApplicationController
 
   def update
     if @booking.update(parse_booking_dates)
-      redirect_to workspace_booking_path(@workspace, @booking), notice: 'Booking successfully updated'
+      redirect_to booking_path(@booking), notice: 'Booking successfully updated'
     else
-      render :new, status: :unprocessable_entity
+      render :show, status: :unprocessable_entity
     end
   end
 
   def destroy
     booking = Booking.find(params[:id])
     booking.destroy
-    redirect_to workspace_bookings_path(@workspace), notice: 'Booking successfully deleted'
+    redirect_to bookings_path, notice: 'Booking successfully deleted'
   end
 
   private
 
-  def find_booking
-    @booking = Booking.find(params[:id])
-  end
-
   def find_workspace
     @workspace = Workspace.find(params[:workspace_id])
+  end
+
+  def find_booking
+    @booking = Booking.find(params[:id])
   end
 
   def booking_params
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def resolve_booking_form_options(booking)
     if booking.new_record?
-      { url: workspace_bookings_path, action: 'create' }
+      { url: bookings_path, action: 'create' }
     else
-      { url: workspace_booking_path, action: 'update' }
+      { url: booking_path, action: 'update' }
     end
   end
 end
